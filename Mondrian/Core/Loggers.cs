765d0e0a212b10033d0e9d38e4c9774bdd29b1d9
Joshua May <joshuarmay@hotmail.com>
2022-09-01 22:04:55 -0700

    Add core lib

---
+++ Mondrian/Core/Loggers.cs
@@ -0,0 +1,22 @@
+﻿namespace Core
+{
+    public abstract class LoggerBase
+    {
+        public abstract void LogMessage(string logString);
+
+        public abstract void LogError(string logString);
+    }
+
+    public class ConsoleLogger : LoggerBase
+    {
+        public override void LogMessage(string logString)
+        {
+            Console.WriteLine(logString);
+        }
+
+        public override void LogError(string logString)
+        {
+            Console.Error.WriteLine(logString);
+        }
+    }
+}
--- Mondrian/Mondrian/Program.cs
+++ Mondrian/Mondrian/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace Mondrian
+﻿using Core;
+
+namespace Mondrian
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            LoggerBase logger = new ConsoleLogger();
+
+            logger.LogMessage("Hello, World!");
         }
     }
 }
