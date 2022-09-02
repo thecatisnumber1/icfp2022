55d061d3bb7acda3f64c5f8acfcbbc3fe7bd28d2
Chris Prichard <prichard.chris@gmail.com>
2022-09-02 14:44:12 -0700

    Added runner class.

---
+++ Mondrian/Mondrian/Runner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core;
+using AI;
+using System.Diagnostics;
+
+namespace Mondrian
+{
+    public class Runner
+    {
+        public static void Run(Args args, LoggerBase logger)
+        {
+            if (args.algorithm == null)
+            {
+                throw new Exception("Specify algorithm to run.");
+            }
+
+            var solver = Solvers.GetSolver(args.algorithm);
+            for (int problemNum = args.minProblemNumber; problemNum <= args.maxProblemNumber; problemNum++)
+            {
+                Stopwatch watch = Stopwatch.StartNew();
+                logger.LogMessage($"Considering problem #{problemNum}");
+                Image problem = Problems.GetProblem(problemNum);
+                Picasso picasso = new Picasso(problem);
+                solver(picasso, args.aiArgs, logger);
+                logger.LogMessage($"Score = {picasso.Score}, elapsed = {watch.Elapsed}");
+                // TODO: Need to add printing the solution that was found.
+            }
+        }
+    }
+}
