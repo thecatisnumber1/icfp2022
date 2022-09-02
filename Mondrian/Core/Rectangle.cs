c761290898dc397715021a04a5f087475eeb5cc0
Josh Snyder <joshsnyder@msn.com>
2022-09-02 12:05:39 -0700

    Added Rectangle class

---
+++ Mondrian/Core/Rectangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class Rectangle
+    {
+        public readonly Point BottomLeft, TopRight;
+
+        public Rectangle(Point bottomLeft, Point topRight)
+        {
+            BottomLeft = bottomLeft;
+            TopRight = topRight;
+        }
+
+        public int Top => TopRight.Y;
+        public int Bottom => BottomLeft.Y;
+        public int Left => BottomLeft.X;
+        public int Right => TopRight.X;
+
+        public Point BottomRight => new Point(Right, Bottom);
+        public Point TopLeft => new Point(Left, Top);
+    }
+}
