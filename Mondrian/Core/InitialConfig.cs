893e7f0a77e6f1245b34c587ccf963726995a3a2
William Pittman <wspittman@gmail.com>
2022-09-03 10:34:12 -0700

    Theoretical initial config file loading

---
+++ Mondrian/Core/InitialConfig.cs
@@ -0,0 +1,17 @@
+﻿namespace Core
+{
+    public class InitialBlockDef
+    {
+        public string blockId;
+        public int[] bottomLeft;
+        public int[] topRight;
+        public byte[] color;
+    }
+
+    public class InitialConfig
+    {
+        public int width;
+        public int height;
+        public InitialBlockDef[] blocks;
+    }
+}
---
+++ Mondrian/Mondrian/InitialConfigs.cs
@@ -0,0 +1,59 @@
+﻿using Core;
+using System.Text.Json;
+
+namespace Mondrian
+{
+    public static class InitialConfigs
+    {
+        private static string? configDir = null;
+        private static int? configCount = null;
+
+        public static InitialConfig? GetInitialConfig(int num)
+        {
+            return LoadFile(LocationFor($"{num}.initial.json"));
+        }
+
+        public static InitialConfig? LoadFile(string path)
+        {
+            string text = File.ReadAllText(path);
+            return JsonSerializer.Deserialize<InitialConfig>(text);
+        }
+
+        public static int InitialConfigCount()
+        {
+            return configCount ??= Directory.GetFiles(ConfigDirectory()).Length;
+        }
+
+        private static string ConfigDirectory()
+        {
+            string FindConfigDirectory()
+            {
+                bool ContainsScores(string dirName)
+                {
+                    var dirs = Directory.GetDirectories(dirName);
+                    return (from dir in dirs select Path.GetFileName(dir)).Contains("InitialConfigs");
+                }
+
+                string dir = ".";
+                while (!ContainsScores(dir))
+                {
+                    dir = Path.Join(dir, "..");
+
+                    if (dir.Length > 100)
+                    {
+                        throw new Exception("Couldn't find Initial Configs");
+                    }
+                }
+
+                return Path.Join(dir, "InitialConfigs");
+            }
+
+            return configDir ??= FindConfigDirectory();
+        }
+
+        private static string LocationFor(string filename)
+        {
+            return Path.Join(ConfigDirectory(), filename);
+        }
+    }
+}
