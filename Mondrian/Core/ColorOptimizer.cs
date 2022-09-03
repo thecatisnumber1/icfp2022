03bc2dad42b687b37e223ebc74fec13216537ba5
Josh Snyder <joshsnyder@msn.com>
2022-09-03 16:55:37 -0700

    Some Junk

---
+++ Mondrian/Core/ColorOptimizer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public class ColorOptimizer
+    {
+        public List<RGBA?> ChooseColors(List<Rectangle> regions)
+        {
+            return new();
+        }
+    }
+}
