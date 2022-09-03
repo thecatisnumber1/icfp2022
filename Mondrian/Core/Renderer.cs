71a29e1113faa11c088f8275df20a5534fb22b13
William Pittman <wspittman@gmail.com>
2022-09-02 19:53:53 -0700

    Core: Big Perf Time

--- Mondrian/Core/Renderer.cs
+++ Mondrian/Core/Renderer.cs
@@ -5,20 +5,77 @@
         private readonly Canvas canvas;
         private readonly Image image;
         private readonly RGBA[,] renderCanvas;
-        private readonly double[,] pixelDiffs;
+        private readonly Dictionary<string, double> blockCost;
+        private readonly double[,] pixelCost;
 
         public Renderer(Canvas canvas, Image image)
         {
             this.canvas = canvas;
             this.image = image;
             renderCanvas = new RGBA[canvas.Width, canvas.Height];
-            pixelDiffs = new double[canvas.Width, canvas.Height];
+            blockCost = new Dictionary<string, double>();
+            pixelCost = new double[canvas.Width, canvas.Height];
+
+            // initialize
+            for (int x = 0; x < canvas.Width; x++)
+            {
+                for (int y = 0; y < canvas.Height; y++)
+                {
+                    renderCanvas[x, y] = new RGBA(255, 255, 255, 255);
+                    pixelCost[x, y] = -1;
+                }
+            }
         }
 
         public int GetImageCost()
         {
-            CanvasToFrame();
-            return ImageDiff();
+            double cost = 0;
+            double alpha = 0.005;
+            var blocks = canvas.Simplify();
+
+            foreach (var block in blocks)
+            {
+                if (!block.HasRendered)
+                {
+                    RenderBlock(block);
+                }
+
+                cost += blockCost[block.ID];
+            }
+
+            return (int)Math.Round(cost * alpha);
+        }
+
+        private void RenderBlock(SimpleBlock block)
+        {
+            block.HasRendered = true;
+            double cost = 0;
+
+            for (var y = block.BottomLeft.Y; y < block.TopRight.Y; y++)
+            {
+                for (var x = block.BottomLeft.X; x < block.TopRight.X; x++)
+                {
+                    bool colorChanged = renderCanvas[x, y] != block.Color;
+                    double pixelDiff = pixelCost[x, y];
+
+                    if (colorChanged)
+                    {
+                        renderCanvas[x, y] = block.Color;
+                        pixelDiff = -1;
+                    }
+
+                    if (pixelDiff < 0)
+                    {
+                        var p1 = image[x, y];
+                        var p2 = renderCanvas[x, y];
+                        pixelCost[x, y] = PixelDiff(p1, p2);
+                    }
+
+                    cost += pixelCost[x, y];
+                }
+            }
+
+            blockCost[block.ID] = cost;
         }
 
         // Was Painter.draw(canvas)
@@ -39,7 +96,7 @@
                             if (renderCanvas[x, y] != block.Color)
                             {
                                 renderCanvas[x, y] = block.Color;
-                                pixelDiffs[x, y] = -1;
+                                pixelCost[x, y] = -1;
                             }
                         }
                     }
@@ -57,14 +114,14 @@
             {
                 for (var x = 0; x < image.Width; x++)
                 {
-                    if (pixelDiffs[x, y] < 0)
+                    if (pixelCost[x, y] < 0)
                     {
                         var p1 = image[x, y];
                         var p2 = renderCanvas[x, y];
-                        pixelDiffs[x, y] = PixelDiff(p1, p2);
+                        pixelCost[x, y] = PixelDiff(p1, p2);
                     }
 
-                    diff += pixelDiffs[x, y];
+                    diff += pixelCost[x, y];
                 }
             }
             
