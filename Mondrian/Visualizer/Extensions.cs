44e73badbc0f180bf2ce61774e3c3b0b517e3e9a
Joshua May <joshuarmay@hotmail.com>
2022-09-03 14:42:38 -0700

    'Lol'

---
+++ Mondrian/Visualizer/Extensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Core;
+
+namespace Visualizer
+{
+    internal static class Extensions
+    {
+        /// <summary>
+        /// Convert a point taken from the viewport to the model, translating screen coordinates to real coordinates
+        /// </summary>
+        public static Point FromViewportToModel(this System.Windows.Point p, int height)
+        {
+            return new Point((int)Math.Round(p.X), height - (int)Math.Round(p.Y));
+        }
+    }
+}
